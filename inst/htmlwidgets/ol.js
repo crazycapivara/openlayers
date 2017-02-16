ecdb7478f50da8a3a6ac12043853958ff6b006f0
skuethe <crazycapivara@gmail.com>
2017-02-16 21:07:56 +0100

    Remove dead code

--- inst/htmlwidgets/ol.js
+++ inst/htmlwidgets/ol.js
@@ -9,17 +9,6 @@ HTMLWidgets.widget({
     // TODO: define shared variables for this instance
     var map = new ol.Map({
       target: el.id
-      /*,
-      layers: [
-        new ol.layer.Tile({
-          source: new ol.source.OSM()
-        })
-      ]*/
-      /*,
-      view: new ol.View({
-        center: ol.proj.fromLonLat([37.41, 8.82]),
-        zoom: 4
-      })*/
     });
 
     return {
@@ -28,12 +17,10 @@ HTMLWidgets.widget({
 
         // TODO: code to render the widget, e.g.
         //el.innerText = x.message;
-        //x.view = true;
 
         if(x.view){
           map.setView(
             new ol.View({
-              //center: ol.proj.fromLonLat([37.41, 8.82]),
               center: ol.proj.fromLonLat([x.view.lon, x.view.lat]),
               zoom: x.view.zoom
             })
