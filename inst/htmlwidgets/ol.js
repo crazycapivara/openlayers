ff872aca26e3ec053a881de9a60db100a99aebcd
skuethe <stefan@fdkii.de>
2017-08-11 22:31:03 +0200

    Move dead code

--- inst/htmlwidgets/ol.js
+++ inst/htmlwidgets/ol.js
@@ -245,24 +245,6 @@ HTMLWidgets.widget({
     });
 
     olWidget.element = el;
-    //helpMe.addContainer(el, "info");
-    // use select interaction instead!?
-    /*
-    map.on("pointermove", function(evt) {
-      //if (evt.dragging) return;
-      console.log("clicked");
-      var feature = map.forEachFeatureAtPixel(evt.pixel,
-      function(feature) {
-          return feature;
-      });
-      //console.log(feature);
-      var property = "NAME";
-      if (feature) {
-        console.log(feature.getId());
-        console.log(feature.get(property));
-      }
-    });
-    */
 
     return {
 
---
+++ sandbox/selectFeatureAtPixel.js
@@ -0,0 +1,18 @@
+//helpMe.addContainer(el, "info");
+    // use select interaction instead!?
+    /*
+    map.on("pointermove", function(evt) {
+      //if (evt.dragging) return;
+      console.log("clicked");
+      var feature = map.forEachFeatureAtPixel(evt.pixel,
+      function(feature) {
+          return feature;
+      });
+      //console.log(feature);
+      var property = "NAME";
+      if (feature) {
+        console.log(feature.getId());
+        console.log(feature.get(property));
+      }
+    });
+    */
