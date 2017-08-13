c9e682e1ac0c3fbc6573fe51c952b3ecb1dd042d
skuethe <stefan@fdkii.de>
2017-08-13 21:28:36 +0200

    Update examples

--- inst/examples/add_geojson_url.R
+++ inst/examples/add_geojson_url.R
@@ -4,13 +4,13 @@ geojson_url <- 'https://openlayers.org/en/v4.2.0/examples/data/geojson/countries
 
 colors <- substr(rainbow(178), 1, 7)
 
-map <- ol() %>%
+map <- ol(options = ol_options(debug = TRUE)) %>%
   add_stamen_tiles() %>%
   add_geojson(filename = geojson_url,
               style = fill_style(colors),
-              options = vector_options(opacity = 0.5),
-              select = select_interaction(property = "name")) %>%
-  add_mouse_position() #%>% add_stamen_tiles("terrain-labels")
+              options = vector_options(opacity = 0.5)) %>%
+  add_mouse_position() %>%
+  add_select() #%>% add_stamen_tiles("terrain-labels")
 
 map
 
