ab99c81a05bd18f22b93acb18f20261c55ee7ee8
skuethe <stefan@fdkii.de>
2017-08-15 00:12:30 +0200

    Update shiny example

--- inst/examples/shiny.R
+++ inst/examples/shiny.R
@@ -3,7 +3,8 @@ library(geojsonio)
 
 view <- fluidPage(
   h1("openleayers"),
-  olOutput("map")
+  olOutput("map"),
+  tableOutput("selected")
 )
 
 server <- function(input, output) {
@@ -12,15 +13,20 @@ server <- function(input, output) {
   })
 
   observeEvent(input$map_select, {
-    print(input$map_select)
+    print(input$map_select %>% as.data.frame())
+  })
+
+  df <- eventReactive(input$map_select, {
+    input$map_select %>% as.data.frame()
   })
 
   output$map <- renderOl({
     ol(options = ol_options(debug = TRUE)) %>% add_stamen_tiles() %>%
-      add_vector_data(us_cities[1:10, ],
-                      style = circle_style(),
-                      select = select_interaction(property = "name"))
+      add_vector_data(us_cities[1:10, ], style = circle_style()) %>%
+      add_select() %>% add_drag_and_drop()
   })
+
+  output$selected <- renderTable({df()})
 }
 
 shinyApp(view, server)
