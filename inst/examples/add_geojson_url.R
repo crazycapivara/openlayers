c1376b009302dde6306a393295c7ff8c8ff7b233
skuethe <stefan@fdkii.de>
2017-08-17 23:28:25 +0200

    Update example

--- inst/examples/add_geojson_url.R
+++ inst/examples/add_geojson_url.R
@@ -10,8 +10,9 @@ map <- ol(options = ol_options(debug = TRUE)) %>%
               style = fill_style(colors),
               options = vector_options(opacity = 0.5)) %>%
   add_mouse_position() %>%
-  add_select(display_properties = list("name")) #%>% add_stamen_tiles("terrain-labels")
+  add_select(display_properties = TRUE) #%>% add_stamen_tiles("terrain-labels")
 
 map
 
 map %>% add_overview_map(collapsed = FALSE) #%>% add_stamen_tiles("terrain-labels")
+
