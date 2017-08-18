ce4cc3c178a5a69bc957881a6621a0e2396a0420
skuethe <stefan@fdkii.de>
2017-08-18 18:50:00 +0200

    Update docs

--- R/icons.R
+++ R/icons.R
@@ -1,4 +1,11 @@
+#' Make icon
+#'
+#' Encode icon file to base64, so that it can be passed as \code{src} parameter
+#' to \code{\link{marker_style}}.
+#'
+#' @param filename filename of icon
+#'
 #' @export
-make_icon <- function(src) {
-  paste("data:image/png;base64,", base64enc::base64encode(src))
+make_icon <- function(filename) {
+  paste("data:image/png;base64,", base64enc::base64encode(filename))
 }
