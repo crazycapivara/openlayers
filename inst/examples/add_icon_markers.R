82d166d3e21406878fe8633f2c402a01332dd5e2
skuethe <stefan@fdkii.de>
2017-08-18 21:19:05 +0200

    Add marker example

---
+++ inst/examples/add_icon_markers.R
@@ -0,0 +1,9 @@
+library(openlayers)
+library(geojsonio)
+
+features <- us_cities[1:10, ]
+colors = ifelse(features$capital, "red", "blue")
+
+ol() %>% add_stamen_tiles() %>% add_stamen_tiles("terrain-labels") %>%
+  add_vector_data(features, style = marker_style(color = colors)) %>%
+  add_select(display_properties = TRUE)
