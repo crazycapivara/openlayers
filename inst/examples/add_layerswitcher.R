a47f877b4efbb89efbd0d04c4ba28cd5ac1f9962
skuethe <stefan@fdkii.de>
2017-08-20 19:25:09 +0200

    Update example

--- inst/examples/add_layerswitcher.R
+++ inst/examples/add_layerswitcher.R
@@ -2,7 +2,7 @@ library(openlayers)
 
 ol(options = list(debug = TRUE)) %>%
   add_stamen_tiles() %>%
-  add_stamen_tiles("terrain-labels") %>%
+  add_stamen_tiles("terrain-labels", options = list(name = "terrain-labels")) %>%
   add_vector_data(us_cities[1:10, ],
                   style = marker_style(anchor = c(0.5, 1.1)) + text_style(property = "name", stroke = stroke_style(color = "white"))) %>%
   add_layer_switcher() %>% add_drag_and_drop()
