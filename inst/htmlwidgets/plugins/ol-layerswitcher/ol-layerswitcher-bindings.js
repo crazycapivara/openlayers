ccb37b5b6f1f1d2671a0d62d4e6aa8409b17473d
skuethe <stefan@fdkii.de>
2017-08-20 13:58:55 +0200

    Refactor

--- inst/htmlwidgets/plugins/ol-layerswitcher/ol-layerswitcher-bindings.js
+++ inst/htmlwidgets/plugins/ol-layerswitcher/ol-layerswitcher-bindings.js
@@ -8,12 +8,12 @@ var olWidget = window.olWidget;
 
   methods.addLayerSwitcher = function() {
     olWidget.layerSwitcher = new ol.control.LayerSwitcher();
- 	  this.addControl(olWidget.layerSwitcher);
- 	  // TODO: do not display control, if no layers are present
- 	  /*
- 	  var layers = this.getLayers().getArray();
- 	  var titles = layers.map(function(l) { return l.get("title"); });
- 	  console.log(titles);
- 	  */
+    this.addControl(olWidget.layerSwitcher);
+    // TODO: do not display control, if no layers are present
+    /*
+    var layers = this.getLayers().getArray();
+    var titles = layers.map(function(l) { return l.get("title"); });
+    console.log(titles);
+    */
   };
 })();
