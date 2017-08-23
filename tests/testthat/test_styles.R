5270d6abaacb3fe3fd13614781e8ba3309a24fc3
skuethe <stefan@fdkii.de>
2017-08-23 20:03:22 +0200

    Refactor style tests

--- tests/testthat/test_styles.R
+++ tests/testthat/test_styles.R
@@ -1,23 +1,24 @@
 context("styles")
 
 test_that("combine styles", {
-  # when
+  # given
   color <- "red"
   width <- 3
+  # when
   style <- fill_style(color) + stroke_style(width = width)
   # then
-  stroke_width <- style$stroke$width
-  fill_color <- style$fill$color
-  # ---
-  expect_equal(stroke_width, 3)
-  expect_equal(fill_color, "red")
+  stroke_width_expected <- 3
+  fill_color_expected <- "red"
+  # assert
+  expect_equal(style$stroke$width, stroke_width_expected)
+  expect_equal(style$fill$color, fill_color_expected)
 })
 
 test_that("check class", {
   # when
   style <- fill_style() + stroke_style()
   # then
-  class_ <- class(style)
-  # ---
-  expect_equal(class_, c("list", "style"))
+  class_expected <- c("list", "style")
+  # assert
+  expect_equal(class(style), class_expected)
 })
