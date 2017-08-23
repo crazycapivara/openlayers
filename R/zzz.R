aff6155234e2c69ddfde2018e3ced8aad88f3217
skuethe <stefan@fdkii.de>
2017-08-23 22:02:37 +0200

    Exclude startup message from code coverage

--- R/zzz.R
+++ R/zzz.R
@@ -1,8 +1,8 @@
-.onAttach <- function(libname, pkgname){
+.onAttach <- function(libname, pkgname){ # nocov start
   fn <- system.file("htmlwidgets/ol.yaml", package = "openlayers")
   ol_js_version <- yaml::yaml.load_file(fn)$dependencies[[1]]$version
   packageStartupMessage(
     pkgname, " ", getNamespaceVersion(pkgname),
     " wrapping openlayersjs v", ol_js_version
   )
-}
+} # nocov end
