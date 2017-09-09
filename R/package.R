efa7d3c4aad572efa30ffb75adb0f04d72bfc79d
skuethe <stefan@fdkii.de>
2017-09-09 16:00:45 +0200

    Update package docs

--- R/package.R
+++ R/package.R
@@ -1,7 +1,17 @@
 #' openlayers
 #'
+#' Makes the open-source JavaScript library \href{https://openlayers.org/}{OpenLayers}
+#' available within R via the htmlwidgets package.
+#'
 #' @docType package
 #' @name openlayers
+#'
+#' @examples \dontrun {
+#'   ol() %>% add_osm_tiles()
+#'
+#'   ol() %>% add_stamen_tiles() %>%
+#'     add_features(geojsonio::us_cities[1:5, ])
+#' }
 #'
 #' @importFrom magrittr %>%
 #' @export %>%
