864e905e0e97b484e56d21c5cc7ae6624ac9c0d4
skuethe <stefan@fdkii.de>
2017-09-21 18:56:31 +0200

    Remove obsolete funcs and update examples in docs

--- R/mvt.R
+++ R/mvt.R
@@ -1,41 +1,5 @@
 ## mapbox vector tiles format
 
-#' @describeIn add_vector_tiles Add mapbox vector tiles (mvt) to the map
-#'
-#' @param ol map widget
-#' @param url url of the vector tiles service
-#' @param attribution attribution
-#' @inheritParams add_geojson
-#'
-#' @examples \dontrun{
-#'   # mapbox
-#'   key <- "your_access_token"
-#'   url <- paste0(
-#'     "https://{a-d}.tiles.mapbox.com/v4/",
-#'     "mapbox.mapbox-streets-v7/",
-#'     "{z}/{x}/{y}.vector.pbf",
-#'     "?access_token=", key
-#'   )
-#'   attribution <- "Add mapbox attribution here"
-#'
-#'   ol() %>%
-#'     add_mapbox_vector_tiles(url, attribution)
-#'
-#'   # style the tiles
-#'   ol() %>%
-#'     add_mapbox_vector_tiles(
-#'       url,
-#'       attribution,
-#'       style = stroke_style(color = "green") + fill_style()
-#'     ) %>%
-#'     set_view(zoom = 14)
-#' }
-#'
-#' @export
-add_mapbox_vector_tiles <- function(ol, url, attribution = NULL, style = NULL, options = layer_options()) {
-  invoke_method(ol, "addVectorTiles", url, attribution, style, options, "MVT")
-}
-
 #' Mapbox attribution
 #'
 #' @export
--- R/vector_tiles.R
+++ R/vector_tiles.R
@@ -1,3 +1,7 @@
+#' Guess vector tiles format
+#'
+#' @param url url of the vt service
+#'
 #' @export
 guess_vt_format <- function(url) {
   if (grepl("\\.json", url)) {
@@ -14,16 +18,53 @@ guess_vt_format <- function(url) {
 #' If you want to create and serve your own vector tiles
 #' you may check \href{http://t-rex.tileserver.ch/}{t-rex}.
 #'
-#' @name add_vector_tiles
-#'
+#' @inheritParams add_geojson
+#' @param url url of the vector tiles service
+#' @param attribution attribution
+#' @param format format of the vector tiles, \code{MVT} (mapbox vector tiles),
+#'   \code{TopoJSON}, \code{GeoJSON} or \code{NULL} (guess format)
 #' @examples \dontrun{
 #'   # osrm
-#'   osrm_url <- "http://router.project-osrm.org/tile/v1/car/tile({x},{y},{z}).mvt"
+#'   osrm <- "http://router.project-osrm.org/tile/v1/car/tile({x},{y},{z}).mvt"
 #'
 #'   ol() %>%
 #'     add_osm_tiles() %>%
-#'     add_vector_tiles(osrm_url, "Add attribution") %>%
+#'     add_vector_tiles(osrm, "Add osrm attribution") %>%
 #'     set_view(zoom = 16)
+#'
+#'   # mapzen
+#'   key <- "your_api_key"
+#'   mapzen <- paste0(
+#'     "https://tile.mapzen.com/mapzen/vector/v1/",
+#'     "roads/{z}/{x}/{y}.topojson",
+#'     "?api_key=", key)
+#'   )
+#'
+#'   ol() %>%
+#'     add_vector_tiles(mapzen, "Add mapzen attribution") %>%
+#'     set_view(zoom = 16)
+#'
+#'   # mapbox
+#'   key <- "your_access_token"
+#'   mapbox <- paste0(
+#'     "https://{a-d}.tiles.mapbox.com/v4/",
+#'     "mapbox.mapbox-streets-v7/",
+#'     "{z}/{x}/{y}.vector.pbf",
+#'     "?access_token=", key
+#'   )
+#'   attribution <- "Add mapbox attribution here"
+#'
+#'   ol() %>%
+#'     add_vector_tiles(mapbox, attribution)
+#'
+#'   # style the tiles
+#'   ol() %>%
+#'     add_vector_tiles(
+#'       url,
+#'       attribution,
+#'       style = stroke_style(color = "green") + fill_style()
+#'      ) %>%
+#'      set_view(zoom = 14)
 #' }
 #'
 #' @export
@@ -33,32 +74,4 @@ add_vector_tiles <- function(ol, url, attribution = NULL, style = NULL,
     format <- guess_vt_format(url)
   }
   invoke_method(ol, "addVectorTiles", url, attribution, style, options, format)
-}
-
-#' @describeIn add_vector_tiles Add topojson vector tiles to the map
-#'
-#' @examples \dontrun{
-#'   # mapzen
-#'   key <- "api_key"
-#'   url <- paste0(
-#'     "https://tile.mapzen.com/mapzen/vector/v1/",
-#'     "roads/{z}/{x}/{y}.topojson",
-#'     "?api_key=", key)
-#'   )
-#'
-#'   ol() %>%
-#'     add_topojson_vector_tiles(url, "Add mapzen attribution") %>%
-#'     set_view(zoom = 16)
-#' }
-#'
-#' @export
-add_topojson_vector_tiles <- function(ol, url, attribution = NULL, style = NULL, options = layer_options()) {
-  invoke_method(ol, "addVectorTiles", url, attribution, style, options, "TopoJSON")
-}
-
-#' @describeIn add_vector_tiles Add geojson vector tiles to the map
-#'
-#' @export
-add_geojson_vector_tiles <- function(ol, url, attribution = NULL, style = NULL, options = layer_options()) {
-  invoke_method(ol, "addVectorTiles", url, attribution, style, options, "GeoJSON")
 }
