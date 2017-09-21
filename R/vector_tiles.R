b3f5d63a914bdfb43a0e85275a72fe8437eedccd
skuethe <stefan@fdkii.de>
2017-09-21 18:32:40 +0200

    Add generic function to add vector tiles and guess format

--- R/vector_tiles.R
+++ R/vector_tiles.R
@@ -1,10 +1,39 @@
+#' @export
+guess_vt_format <- function(url) {
+  if (grepl("\\.json", url)) {
+    return("GeoJSON")
+  }
+  if (grepl("\\.topojson", url)) {
+    return("TopoJSON")
+  }
+  "MVT"
+}
+
 #' Add vector tiles to the map
 #'
 #' If you want to create and serve your own vector tiles
 #' you may check \href{http://t-rex.tileserver.ch/}{t-rex}.
 #'
 #' @name add_vector_tiles
-NULL
+#'
+#' @examples \dontrun{
+#'   # osrm
+#'   osrm_url <- "http://router.project-osrm.org/tile/v1/car/tile({x},{y},{z}).mvt"
+#'
+#'   ol() %>%
+#'     add_osm_tiles() %>%
+#'     add_vector_tiles(osrm_url, "Add attribution") %>%
+#'     set_view(zoom = 16)
+#' }
+#'
+#' @export
+add_vector_tiles <- function(ol, url, attribution = NULL, style = NULL,
+                             options = layer_options(), format = NULL) {
+  if (is.null(format)) {
+    format <- guess_vt_format(url)
+  }
+  invoke_method(ol, "addVectorTiles", url, attribution, style, options, format)
+}
 
 #' @describeIn add_vector_tiles Add topojson vector tiles to the map
 #'
