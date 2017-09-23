bbb09908920ba4e79e4b4451e4901375dd9e26d6
skuethe <stefan@fdkii.de>
2017-09-23 19:15:36 +0200

    Add styles for mapbox in vector tiles debug mode

--- inst/htmlwidgets/ol.js
+++ inst/htmlwidgets/ol.js
@@ -28,6 +28,11 @@ var ol = window.ol;
             color = "red";
             break;
         }
+        // mapbox
+        var class_ = feature.get("class");
+        if (class_ === "primary") { color = "blue"; }
+        else if (class_ === "secondary") { color = "green"; }
+        else if (class_ === "street") { color = "yellow"; }
         return new ol.style.Style({
           stroke: new ol.style.Stroke({ color: color, width: 2 })
         });
