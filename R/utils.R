12a409e08857a7ac55df4599c0e88124335cb0e1
skuethe <crazycapivara@gmail.com>
2018-01-08 21:57:10 +0100

    Add utility func to convert variable names to camel case

--- R/utils.R
+++ R/utils.R
@@ -1,8 +1,12 @@
-invoke_method <- function(ol, method_name, ...){
+invoke_method <- function(ol, method_name, ...) {
   n <- length(ol$x$calls)
   ol$x$calls[[n+1]] <- list(
     method = method_name,
     args = list(...)
   )
   ol
+}
+
+camel_case <- function(x) {
+  gsub("_(\\w?)", "\\U\\1", x, perl = TRUE)
 }
