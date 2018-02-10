bbb40fa30f3eadf595f04aceda46fba7cddc6b88
skuethe <crazycapivara@gmail.com>
2018-02-10 18:48:31 +0100

    Add another resolution example

--- inst/examples/resolution.R
+++ inst/examples/resolution.R
@@ -1,5 +1,18 @@
 # resolution example
 
+# TODO: parse options parameters so that 'max_resoltion' can be used!
 ol(options = list(debug = TRUE)) %>%
   add_stamen_tiles() %>%
   add_stamen_tiles("terrain-labels", options = layer_options(maxResolution = 13000))
+
+library(geojsonio)
+
+cities <- us_cities[1:5, ]
+
+# zoom in and out to show text or icons
+ol(options = list(debug = TRUE)) %>%
+  add_stamen_tiles() %>%
+  add_features(cities, style = icon_style(),
+               options = list(minResolution = 5000)) %>%
+  add_features(cities, style = text_style(property = "name"),
+               options = list(maxResolution = 5000))
