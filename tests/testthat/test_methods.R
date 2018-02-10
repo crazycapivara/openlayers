1a9571c02ef8e810bce991134233d43a91c338be
skuethe <crazycapivara@gmail.com>
2018-02-10 21:21:08 +0100

    Add test for 'setView'

--- tests/testthat/test_methods.R
+++ tests/testthat/test_methods.R
@@ -1,12 +1,16 @@
 context("methods")
 
+get_call <- function(map) {
+  map$x$calls[[1]]
+}
+
 test_that("osm tiles", {
   # when
   map <- ol() %>% add_osm_tiles()
   # then
   method_expected <- "addOSMTiles"
   # assert
-  expect_equal(map$x$calls[[1]]$method, method_expected)
+  expect_equal(get_call(map)$method, method_expected)
 })
 
 test_that("stamen tiles", {
@@ -16,7 +20,20 @@ test_that("stamen tiles", {
   method_expected <- "addStamenTiles"
   layer_expected <- "watercolor"
   # assert
-  calls <- map$x$calls[[1]]
-  expect_equal(calls$method, method_expected)
-  expect_equal(calls$args[[1]], layer_expected)
+  call <- get_call(map)
+  expect_equal(call$method, method_expected)
+  expect_equal(call$args[[1]], layer_expected)
+})
+
+test_that("set view", {
+  # when
+  zoom <- 8
+  map <- ol() %>% set_view(zoom = zoom)
+  # then
+  method_expected <- "setView"
+  zoom_expected <- 8
+  # assert
+  call <- get_call(map)
+  expect_equal(call$method, method_expected)
+  expect_equal(call$args[[3]], zoom_expected)
 })
