828ab86656bd68b6a9795a31ab20444aa816b449
skuethe <crazycapivara@gmail.com>
2018-10-16 23:38:27 +0200

    Add legend examples

---
+++ inst/examples/legend.R
@@ -0,0 +1,27 @@
+library("openlayers")
+library("sf")
+
+nc <- system.file("shape/nc.shp", package = "sf") %>% st_read(quiet = TRUE)
+nc$color <- ifelse(nc$AREA > 0.2, "green", "yellow")
+
+map <- ol() %>%
+  add_stamen_tiles() %>%
+  add_features(data = nc, style = fill_style(nc$color) + stroke_style("blue", 1), popup = nc$AREA)
+
+map %>%
+  add_legend(
+    colors = c("yellow", "green"),
+    labels = c("0 - 0.2", "> 0.2"),
+    title = "area",
+    style = list(bottom = "50px", opacity = 0.7)
+  ) %>%
+  add_scale_line()
+
+map %>%
+  add_legend(
+    colors = c("yellow", "green"),
+    labels = c("0 - 0.2", "> 0.2"),
+    title = "area",
+    overlay = FALSE
+  ) %>%
+  add_overview_map()
