f459a32124bb0e61d8da78a14c277294075e38d3
skuethe <crazycapivara@gmail.com>
2018-10-29 18:50:15 +0100

    Update example

--- inst/examples/shiny-render-ui.R
+++ inst/examples/shiny-render-ui.R
@@ -11,13 +11,17 @@ server <- function(input, output) {
   output$map <- renderOl({
     ol(options = ol_options(debug = TRUE)) %>%
       add_stamen_tiles() %>%
-      add_features(us_cities[1:10, ], style = circle_style()) %>%
-      add_select() %>%
-      add_drag_and_drop()
+      add_features(
+        us_cities[1:10, ],
+        style = circle_style(),
+        options = layer_options(name = "us_cities")) %>%
+      add_select(display_properties = TRUE) %>%
+      add_drag_and_drop() %>%
+      add_layer_switcher()
   })
 
   output$ui <- renderUI({
-    olOutput("map", height = "800px")
+    olOutput("map", height = "600px")
   })
 }
 
